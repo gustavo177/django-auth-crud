b7b5690431c1d68cd0013e6c98236c5bac7c1d1e
gustavo177 <gtvadf177@hotmail.com>
2022-09-20 18:15:03 -0500

    first commit

---
+++ tasks/admin.py
@@ -0,0 +1,8 @@
+from venv import create
+from django.contrib import admin
+from .models import Task
+
+class TaskAdmin(admin.ModelAdmin):
+    readonly_fields = ("created",)
+# Register your models here.
+admin.site.register(Task, TaskAdmin)
